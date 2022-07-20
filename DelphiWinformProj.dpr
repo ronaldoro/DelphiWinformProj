021119bef5de0182265e13a1b3ff49f439988e95
wnsh3131@naver.com <wnsh3131@naver.com>
2022-07-21 00:04:10 +0900

    winform test

--- DelphiWinformProj.dpr
+++ DelphiWinformProj.dpr
@@ -2,13 +2,15 @@ program DelphiWinformProj;
 
 uses
   Vcl.Forms,
-  Unit3 in 'Unit3.pas' {Form3};
+  Unit2 in 'Unit2.pas' {Form2},
+  Unit1 in 'Unit1.pas' {Form1};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm3, Form3);
+  //Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TForm2, Form2);
   Application.Run;
 end.
